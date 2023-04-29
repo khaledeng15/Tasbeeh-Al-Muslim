30ad8e43f6e2e76ad567419dcbbc00037e0623fe
khaledeng15 <khaled_eng15@hotmail.com>
2023-04-29 23:50:07 +0300

    Add share app and rate app #13

--- lib/screens/ContactusScreen/ContactusScreen.dart
+++ lib/screens/ContactusScreen/ContactusScreen.dart
@@ -1,8 +1,15 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:in_app_review/in_app_review.dart';
+import 'package:rect_getter/rect_getter.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../../Bloc/cubit/ThemeAppCubit.dart';
 import '../../main.dart';
+
+enum Availability { loading, available, unavailable }
 
 class ContactusScreen extends StatefulWidget {
   @override
@@ -10,10 +17,49 @@ class ContactusScreen extends StatefulWidget {
 }
 
 class ContactusScreenState extends State<ContactusScreen> {
+  final InAppReview _inAppReview = InAppReview.instance;
+  final globalKey = RectGetter.createGlobalKey();
+
+  String _appStoreId =
+      'https://apps.apple.com/us/app/tsbyh-almslm/id739018955?ls=1';
+  String _microsoftStoreId = '';
+  Availability _availability = Availability.loading;
+
   @override
   void initState() {
     super.initState();
+    checkRating();
   }
+
+  void checkRating() {
+    (<T>(T? o) => o!)(WidgetsBinding.instance).addPostFrameCallback((_) async {
+      try {
+        final isAvailable = await _inAppReview.isAvailable();
+
+        setState(() {
+          // This plugin cannot be tested on Android by installing your app
+          // locally. See https://github.com/britannio/in_app_review#testing for
+          // more information.
+          _availability = isAvailable && !Platform.isAndroid
+              ? Availability.available
+              : Availability.unavailable;
+        });
+      } catch (_) {
+        setState(() => _availability = Availability.unavailable);
+      }
+    });
+  }
+
+  void _setAppStoreId(String id) => _appStoreId = id;
+
+  void _setMicrosoftStoreId(String id) => _microsoftStoreId = id;
+
+  Future<void> _requestReview() => _inAppReview.requestReview();
+
+  Future<void> _openStoreListing() => _inAppReview.openStoreListing(
+        appStoreId: _appStoreId,
+        microsoftStoreId: _microsoftStoreId,
+      );
 
   @override
   Widget build(BuildContext context) {
@@ -22,32 +68,102 @@ class ContactusScreenState extends State<ContactusScreen> {
         child: Scaffold(
           appBar: AppBar(title: Text(" عن التطبيق")),
           body: Padding(
-              padding: const EdgeInsets.only(top: 20, bottom: 50),
-              child: Column(children: [
-                rowViewIcon("البريد الالكترونى :", "khaled.he15@gmail.com",
-                    Icon(Icons.mail, size: 20), () {
-                  requestUrl("mailto:khaled.he15@gmail.com");
-                }),
-                rowViewIcon(
-                    "الهاتف :", "+20 103 072 2286", Icon(Icons.call, size: 20),
-                    () {
-                  requestUrl("tel:+201030722286");
-                }),
-                rowViewIcon(" تحميل كود التطبيق بالكامل", "",
-                    Icon(Icons.app_shortcut, size: 20), () {
-                  requestUrl(
-                      "https://github.com/khaledeng15/Tasbeeh-Al-Muslim");
-                }),
-                rowViewIcon("صفحه التطبيق", "", Icon(Icons.pageview, size: 20),
-                    () {
-                  requestUrl("https://www.cybeasy.com/Tasbeeh-Al-Muslim");
-                }),
-                rowViewIcon(" الوضع المظلم", "",
-                    Icon(Icons.dark_mode_outlined, size: 20), () {
-                  ThemeAppCubit.get(context).ChangeAppMode();
-                  setState(() {});
-                }),
-              ])),
+              padding: const EdgeInsets.only(
+                  top: 20, bottom: 50, left: 20, right: 20),
+              child: SingleChildScrollView(
+                child: Column(children: [
+                  SizedBox(
+                      width: double.maxFinite,
+                      height: 30,
+                      child: Text(
+                        "اتصل بنا",
+                        style: Theme.of(context).textTheme.bodyLarge?.apply(
+                            color: Theme.of(context)
+                                .colorScheme
+                                .onSecondaryContainer),
+                        textAlign: TextAlign.right,
+                      )),
+                  Card(
+                    child: Column(
+                      children: [
+                        rowViewIcon(
+                            "البريد الالكترونى", "", Icon(Icons.mail, size: 20),
+                            () {
+                          requestUrl("mailto:khaled.he15@gmail.com");
+                        }),
+                        rowViewIcon("الهاتف :", "+20 103 072 2286",
+                            Icon(Icons.call, size: 20), () {
+                          requestUrl("tel:+201030722286");
+                        }),
+                        rowViewIcon("واتس :", "+20 103 072 2286",
+                            Icon(Icons.messenger_outline, size: 20), () {
+                          requestUrl("whatsapp://send?phone=+201030722286");
+                        }),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Column(children: [
+                    SizedBox(
+                        width: double.maxFinite,
+                        height: 30,
+                        child: Text(
+                          "عن التطبيق",
+                          style: Theme.of(context).textTheme.bodyLarge?.apply(
+                              color: Theme.of(context)
+                                  .colorScheme
+                                  .onSecondaryContainer),
+                          textAlign: TextAlign.right,
+                        )),
+                    Card(
+                        child: Column(children: [
+                      rowViewIcon(" تحميل كود التطبيق بالكامل", "",
+                          Icon(Icons.app_shortcut, size: 20), () {
+                        requestUrl(
+                            "https://github.com/khaledeng15/Tasbeeh-Al-Muslim");
+                      }),
+                      rowViewIcon(
+                          "صفحه التطبيق", "", Icon(Icons.pageview, size: 20),
+                          () {
+                        requestUrl("https://www.cybeasy.com/Tasbeeh-Al-Muslim");
+                      }),
+                      rowViewIcon(
+                          "قيم التطبيق", "", Icon(Icons.rate_review, size: 20),
+                          () {
+                        if (_availability == Availability.available) {
+                          _inAppReview.requestReview();
+                        }
+                      }),
+                      rowViewIcon(
+                          "انشر التطبيق", "", Icon(Icons.share, size: 20),
+                          () async {
+                        String shareTxt = "قم بتحميل برنامج تسبيح المسلم \n" +
+                            "https://www.cybeasy.com/Tasbeeh-Al-Muslim/";
+                        if (Platform.isIOS) {
+                          var rect = RectGetter.getRectFromKey(globalKey);
+                          await Share.share(
+                            shareTxt,
+                            sharePositionOrigin: Rect.fromLTWH(
+                                rect!.left + 40, rect.top + 20, 2, 2),
+                          );
+                        } else {
+                          await Share.share(
+                            shareTxt,
+                          );
+                        }
+                      }),
+                    ]))
+                  ]),
+                  // Divider(),
+                  rowViewIcon(" الوضع المظلم", "",
+                      Icon(Icons.dark_mode_outlined, size: 20), () {
+                    ThemeAppCubit.get(context).ChangeAppMode();
+                    setState(() {});
+                  }),
+                ]),
+              )),
         ));
   }
 
@@ -69,7 +185,7 @@ class ContactusScreenState extends State<ContactusScreen> {
           margin: EdgeInsets.symmetric(horizontal: 10, vertical: 05),
           padding: EdgeInsets.symmetric(horizontal: 10),
           height: 50,
-          child: Card(
+          child: Container(
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -14,6 +14,7 @@ import firebase_crashlytics
 import firebase_messaging
 import flutter_local_notifications
 import flutter_native_timezone
+import in_app_review
 import just_audio
 import nb_utils
 import package_info_plus
@@ -33,6 +34,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterNativeTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterNativeTimezonePlugin"))
+  InAppReviewPlugin.register(with: registry.registrar(forPlugin: "InAppReviewPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   NbUtilsPlugin.register(with: registry.registrar(forPlugin: "NbUtilsPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
